cb8811adbde57af154208a829d435e7a7b8a3f60
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-02 16:11:06 +0700

    Adding string data type.

---
+++ src/main/kotlin/datatype/stringtype/String.kt
@@ -0,0 +1,65 @@
+package datatype.stringtype
+
+fun main() {
+
+    //String
+    //String with quotation mark
+    var firstName: String = "Taufik"
+    var lastName: String = "Hidayat"
+    var fullName: String = "Taufik Hidayat"
+
+    //For quotation mark
+    println(firstName)
+    println(lastName)
+    println(fullName)
+
+    //String with triple quotation mark for more than one line
+    var address1: String = """
+        Jakarta Pusat,
+        DKI Jakarta,
+        Indonesia
+    """
+
+    //For triple quotation mark
+    print("Your address: $address1")
+
+    //String with triple quotation mark using trim margin
+    // "|" this called is pipe mark (READ: pipe mark)
+    var address2: String = """
+        |Jakarta Pusat,
+        |DKI Jakarta,
+        |Indonesia
+    """.trimMargin()
+
+    //Print using pipe mark
+    println(address2)
+
+    //String with triple quotation mark using trim margin
+    // ">" this called is more than mark
+    var address3: String = """
+        >Jakarta Pusat,
+        >DKI Jakarta,
+        >Indonesia
+    """.trimMargin(">")
+
+    //Print using more than mark
+    println(address3)
+
+    //String with triple quotation mark using trim indent
+    var address4: String = """
+        Jakarta Pusat,
+        DKI Jakarta,
+        Indonesia
+    """.trimIndent()
+
+    //Print using more than mark
+    println("Alamat 4: $address4")
+
+    //Merge strings
+    var fullNames:String = "Nama lengkap: $firstName $lastName"
+    println(fullNames)
+
+    //Strings with complex String Template
+    var charOfFullName = "Total char pada $fullName adalah ${fullName.length} char"
+    println(charOfFullName)
+}
