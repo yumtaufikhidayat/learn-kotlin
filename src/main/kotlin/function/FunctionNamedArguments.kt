14294a6710f7d0770a28276679c0d2a408cba0eb
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-11 11:02:53 +0700

    Adding function named argument.

---
+++ src/main/kotlin/function/FunctionNamedArguments.kt
@@ -0,0 +1,19 @@
+package function
+
+fun main() {
+
+    //Call function with random parameter
+    fullName(
+        firstName = "Eko",
+        lastName = "Khannedy",
+        middleName = "Kurniawan"
+    )
+}
+
+fun fullName(
+    firstName: String,
+    middleName: String,
+    lastName: String
+) {
+    println("Hello, $firstName $middleName $lastName")
+}
