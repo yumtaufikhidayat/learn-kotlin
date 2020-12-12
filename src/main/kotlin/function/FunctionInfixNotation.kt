1387fa84f4ca2d14745171dbd5167b5e62aeca14
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-12 19:10:08 +0700

    Adding varargs function and function infix notation.

---
+++ src/main/kotlin/function/FunctionInfixNotation.kt
@@ -0,0 +1,22 @@
+package function
+
+fun main() {
+
+    //Access infix function wihout . (dot) mark
+    val result = "Taufik" to "UP"
+    println(result)
+
+    //Access infix function using . (dot) mark
+    val result2 = "Taufik".to("LOW")
+    println(result2)
+}
+
+//This infix function must be using one parameter, neither empty or more than one
+//If we want to access this function, it is not required to use . (dot) mark
+infix fun String.to(type: String): String {
+    return if (type == "UP") {
+        this.toUpperCase()
+    } else {
+        this.toLowerCase()
+    }
+}
---
+++ src/main/kotlin/function/FunctionVarargsParameter.kt
@@ -0,0 +1,44 @@
+package function
+
+fun main() {
+
+    //Data for hitungTotal function
+    val values = arrayOf(10, 10, 10, 10)
+    val result = hitungTotal(values)
+    println("Hasilnya adalah $result")
+
+    //Data for hitungTotalVarargs function
+    val resultVarArgs = hitungTotalVarargs("Taufik", 10, 10, 10, 10, 10, 10)
+    println("Hasil varargs-nya adalah $resultVarArgs")
+
+    //Data for hitungTotalVarargs function with empty input for varargs
+    val resultEmptyVarArgs = hitungTotalVarargs("Taufik")
+    println("Hasil empty varargs-nya adalah $resultEmptyVarArgs")
+}
+
+//Common function with array as parameter and Int as return type
+fun hitungTotal(values: Array<Int>): Int {
+
+    var total = 0
+
+    for (value in values) {
+        total += value
+    }
+
+    return total
+}
+
+//Function with vararg as parameter
+//Vararg must be placed in last parameter, neither in first parameter nor in middle parameter
+//If using vararg, it would be converted to array automatically
+//We can also use an empty input for vararg parameter
+fun hitungTotalVarargs(name: String, vararg values: Int): Int {
+
+    var total = 0
+
+    for (value in values) {
+        total += value
+    }
+
+    return total
+}
