e553d4ff8fe0fc2dc484b19bc8cd0ab8dd5b424c
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-12 18:28:03 +0700

    Updating extension function comments.

--- src/main/kotlin/function/ExtensionFunction.kt
+++ src/main/kotlin/function/ExtensionFunction.kt
@@ -9,8 +9,8 @@ fun main() {
     "Hidayat".printHello()
 }
 
-//$this means to access data of function. It returns String type
+//$this means to access data of function. This method returns String type
 fun String.hello():String = "Hello, $this"
 
-//$this means to access data of function. It doesn't returns any return type
+//$this means to access data of function. This method doesn't returns any return type
 fun String.printHello(): Unit = println("Hello, $this")
