93a8f2796a5a104e669ee241ac49e5f607d53db3
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-14 12:25:06 +0700

    Adding anonymous function.

---
+++ src/main/kotlin/function/AnonymousFunction.kt
@@ -0,0 +1,24 @@
+package function
+
+fun main() {
+
+    fun hello(name: String, transformer: (String) -> String): String {
+        return "Hello, ${transformer(name)}"
+    }
+
+    val upper = fun(value: String): String {
+        return if (value == "") {
+            "UPS!"
+        } else {
+            value.toUpperCase()
+        }
+    }
+
+    println(hello("Taufik Hidayat", upper))
+    println(hello("", upper))
+
+    //We can input anonymous function as parameter
+    println(hello("Hidayat", fun(value: String): String {
+        return value.toLowerCase()
+    }))
+}
