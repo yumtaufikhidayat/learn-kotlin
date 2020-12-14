49fb6f131398f6f8cbd572e842aadf38dc64fd5f
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-14 12:12:36 +0700

    Adding higher order function.

---
+++ src/main/kotlin/function/HigherOrderFunction.kt
@@ -0,0 +1,32 @@
+package function
+
+fun main() {
+
+    //Make some variables and convert to lambda expression
+    //The variable that converted as lambda expression will be using as parameter
+    val toUpperCase = {value:String -> value.toUpperCase()}
+    val toLowerCase = {value:String -> value.toLowerCase()}
+
+    println(hello("Taufik", toUpperCase))
+    println(hello("Taufik", toLowerCase))
+
+    //Trailing lambda
+    //It makes easy to read
+    //But it must be placed at the end where lambda expression placed
+    val result1 = hello("Taufik") {
+            value:String -> value.toUpperCase()
+    }
+
+    val result2 = hello("Hidayat") {
+            value:String -> value.toLowerCase()
+    }
+
+    println(result1)
+    println(result2)
+}
+
+//This method will convert lambda expression as parameter
+//Lambda expression not only as function, but also as parameter
+fun hello(name: String, transformer: (String) -> String): String {
+    return "Hello, ${transformer(name)}"
+}
