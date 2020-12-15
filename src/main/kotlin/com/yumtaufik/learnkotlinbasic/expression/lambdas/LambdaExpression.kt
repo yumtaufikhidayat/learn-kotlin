39a0cb507f9c5c3735ea19ac38458101cdc3c771
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-15 07:19:05 +0700

    Updating lambda expression file.

--- src/main/kotlin/com/yumtaufik/learnkotlinbasic/expression/lambdas/LambdaExpression.kt
+++ src/main/kotlin/com/yumtaufik/learnkotlinbasic/expression/lambdas/LambdaExpression.kt
@@ -1,11 +1,11 @@
-package com.yumtaufik.learnkotlinbasic.expression.lambdas
+package cexpression.lambdas
 
 fun main() {
 
     //With lambda, we are not required to type return as return type
     //The lambda's body is automatically convert as return
 
-    //Variable with lambda com.yumtaufik.learnkotlinbasic.expression using two parameters
+    //Variable with lambda expression using two parameters
     //(String, String) -> They are two as input but also parameters with String data type.
     //String -> It is return as String type
     val contohLambda: (String, String) -> String = { firstName: String, lastName: String ->
@@ -15,21 +15,21 @@ fun main() {
 
     println(contohLambda("Taufik", "Hidayat"))
 
-    //variable with lambda com.yumtaufik.learnkotlinbasic.expression using it as single parameter
+    //variable with lambda expression using it as single parameter
     val sayHelloIt: (String) -> String = {
         "Hello, $it"
     }
 
     println(sayHelloIt("Taufik"))
 
-    //Or we can use lambda com.yumtaufik.learnkotlinbasic.expression without using it but using parameter name
+    //Or we can use lambda expression without using it but using parameter name
     val sayHello: (String) -> String = {value: String ->
         "Hello, $value"
     }
 
     println(sayHello("Hidayat"))
 
-    //Making lambda com.yumtaufik.learnkotlinbasic.expression from existing com.yumtaufik.learnkotlinbasic.function was called as method reference
+    //Making lambda expression from existing function was called as method reference
     val toUpperCase: (String) -> String = ::toUpper
     println(toUpperCase("Taufik Hidayat"))
 }
