f5638870a02cac063dfe1d377b6df5d500121f96
yumtaufikhidayat <yumtaufik1997@gmail.com>
2020-12-15 07:40:08 +0700

    Adding main parameter.

---
+++ src/main/kotlin/com/yumtaufik/learnkotlinbasic/parameter/MainParameter.kt
@@ -0,0 +1,13 @@
+package com.yumtaufik.learnkotlinbasic.parameter
+
+/*
+By default, there is no output printed
+But we can configure to print out data
+Just access Run/Debug Configuration -> Edit Configurations -> Fill Program Arguments
+We can input anything there as we want
+*/
+fun main(args: Array<String>) {
+    for (value in args) {
+        println(value)
+    }
+}
