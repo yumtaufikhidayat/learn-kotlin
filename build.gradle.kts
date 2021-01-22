b6df7fa05eb4675e0ca468fa6a69bd72baa5cca0
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-01-22 11:34:19 +0700

    Updating project to new Kotlin version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 dependencies {
     testImplementation(kotlin("test-junit"))
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
@@ -21,4 +23,12 @@ tasks.test {
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
