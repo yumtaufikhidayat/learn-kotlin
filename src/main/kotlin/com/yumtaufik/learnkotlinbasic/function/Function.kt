1317bb21b98c2ced2f77479a8f2f07cbadd676f9
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-07-22 08:14:53 +0700

    Updating function topic and function parameter topic.

--- src/main/kotlin/com/yumtaufik/learnkotlinbasic/function/Function.kt
+++ src/main/kotlin/com/yumtaufik/learnkotlinbasic/function/Function.kt
@@ -2,9 +2,15 @@ package com.yumtaufik.learnkotlinbasic.function
 
 fun main() {
     helloWorld()
+    peopleName()
 }
 
 fun helloWorld() {
     println("Hello world")
     println("Selamat belajar Kotlin")
+    peopleName()
+}
+
+fun peopleName(){
+    println("Hello, my name is Taufik Hidayat")
 }
--- src/main/kotlin/com/yumtaufik/learnkotlinbasic/function/FunctionParameter.kt
+++ src/main/kotlin/com/yumtaufik/learnkotlinbasic/function/FunctionParameter.kt
@@ -5,6 +5,7 @@ fun main() {
     sayHello("Taufik", "Hidayat")
     sayHello("Rizki", null)
     sayHello("Aldo", "Bimantara")
+    helloName("Taufik Hidayat", 17)
 }
 
 //Function with parameter
@@ -14,4 +15,8 @@ fun sayHello(firstName: String, lastName: String?) {
     } else {
         println("Hello $firstName $lastName")
     }
+}
+
+fun helloName(name: String, age: Int) {
+    println("Hello $name, my age is $age")
 }
